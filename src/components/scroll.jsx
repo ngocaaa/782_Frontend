671dbf54b96173a4ccee2d06029e8e4719e31f76
bavneetdeakin <bavneet@deakin.edu.au>
2019-05-23 19:31:15 +1000

    scroll1commit

--- src/components/scroll.jsx
+++ src/components/scroll.jsx
@@ -11,7 +11,9 @@ class App extends React.Component {
 class Content extends React.Component {
     render(){
         return (
-            <div onClick={this.handleSomeAction.bind(this)}> Some long content </div>
+            <div onClick={this.handleSomeAction.bind(this)}> LERI is a content management system for researchers. The product is divided into 3 main applications, the frontend, the backend application, and the researchers' panel application. - The learning resources are divided into 4 categories namely programs, modules, activities, tasks **(PMAT)**. All the entities are individual and hence highly reusable. A program can have multiple modules and a module can have multiple tasks and activities. 
+
+            </div>
         );
     }
  
